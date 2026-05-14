327d2ae19054e67bb4997f2a674a99576325d4a2
SINGH <siddharth.aa.singh@accenture.com>
2026-05-15 03:00:18 +0530

    Add comments file and update identifier rules

---
+++ Chapter_01_Basics/03_Identifier_Rules.js
@@ -0,0 +1,30 @@
+// Starting with letters
+let name = "John";
+let firstName = "Alice";
+let myVar123 = 42;
+
+// Starting with underscore
+let _private = "secret";
+let __dunder = "special";
+
+// Starting with dollar sign
+let $jQuery = "value";
+let $total = 100;
+
+// Mixed (letter, numbers, underscore, dollar)
+let user_name = "Bob";
+let $user_123 = "value";
+let _$mixed$ = true;
+
+// camelCase convention
+let userName = "Charlie";
+let getUserData = function () { };
+
+// PascalCase convention (for classes/constructors)
+class User { }
+function MyClass() { }
+
+// snake_case convention (commonly used in constants and database fields)
+let first_name = "David";
+let user_full_name = "John Doe";
+
---
+++ Chapter_01_Basics/04_Comments.js
@@ -0,0 +1,9 @@
+// this is a single line comment
+
+/* this is a
+multi line comment
+*/
+
+
+// let x =10; // command+forward-slash
+// y = 10;
