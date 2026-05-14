3ea2ebb4e35c21e920a87d1ccbe15e43158160cb
SINGH <siddharth.aa.singh@accenture.com>
2026-05-15 01:20:14 +0530

    Initial commit

---
+++ Chapter_01_Basics/01_Basics.js
@@ -0,0 +1,6 @@
+console.log("Hello World!");
+let a=10;
+console.log(a);
+
+//command to see bytecode
+//node --print-bytecode 01_Basics.js
---
+++ Chapter_01_Basics/02_JS_commands.js
@@ -0,0 +1,4 @@
+console.log(process.platform); //win32,darwin,linux
+console.log(process.arch); //x64,arm64
+console.log(process.version); //v18.16.0
+
